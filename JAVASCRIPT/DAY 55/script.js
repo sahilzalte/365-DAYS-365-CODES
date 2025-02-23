6a07ebe0b69cc03ba093a273dd1d756f4426f2e7
Sahil Zalte <zaltesahil94@gmail.com>
2025-02-23 16:26:56 +0530

    Day 55

---
+++ JAVASCRIPT/DAY 55/script.js
@@ -0,0 +1,45 @@
+// console.log("hey This is Day 55")
+
+// var a = 5;
+// var b = 6;
+// var f = "Sahil"
+// // var 34ram = "Ram"; < -- Invalid Variable Name
+// console.log(a + b)
+
+// console.log(typeof a, typeof c)
+
+// const a1 = 7;
+
+// console.log(a1)
+// {
+//     let a = 55;
+//     // var f = 45;
+//     console.log(a);
+//     console.log(f);
+
+// }
+// // console.log(a);
+// console.log(f);
+
+
+let a = "sahil";
+let b = 22;
+let c = 3.14;
+const d = true;
+let e = undefined;
+let f = null;
+
+
+console.log(a, b, c, d, e, f);
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f);
+
+let o = {
+    name: "Sahil",
+    "college name": "Snd",
+    is_Handsome: true,
+}
+// console.log(o);
+o.salary = "100Crores"
+// console.log(o)
+o.salary = "500Crores"
+console.log(o)
