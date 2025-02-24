1cecb400ae49565ab7d0420a486707744d8f65e6
Sahil Zalte <zaltesahil94@gmail.com>
2025-02-24 22:49:31 +0530

    Day 56

---
+++ JAVASCRIPT/DAY 56/index.js
@@ -0,0 +1,38 @@
+console.log("Hello I am Conditional Statement");
+
+let age = 1
+// let grace = 2
+// age += grace 
+// console.log(age);
+
+// console.log("Arithmatic Operator");
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+
+if (age == 18) {
+    console.log("You Can Drive");
+}
+else if (age == 0) {
+    console.log("Are You Joking?");
+}
+else if (age == 1) {
+    console.log("Are You Joking now also?");
+
+}
+else {
+    console.log("You cannot Drive");
+}
+
+
+
+// Ternary Operator
+let a = 10, b = 19
+c = a > b ? a - b : a + b
+
+console.log(c);
