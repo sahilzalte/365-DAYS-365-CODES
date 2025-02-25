b91d75bb8e69c484f64f35bd7cd24118cb35506e
Sahil Zalte <zaltesahil94@gmail.com>
2025-02-25 22:36:34 +0530

    Day 57

---
+++ JAVASCRIPT/DAY 57/index.js
@@ -0,0 +1,40 @@
+console.log("Loops");
+
+
+let a = 1
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+
+// }
+
+
+// let obj = {
+//     name: "Sahil",
+//     role: "Programmer",
+//     collage: "SND"
+// }
+
+// for (const key in obj) {
+//     console.log(key)
+// }
+
+// for (const c of "Sahil") {
+//     console.log(c)
+// }
+
+// let i = 0
+// while (i < 8389) {
+//     console.log(i)
+//     i++
+// }
+
+let i = 10
+do {
+    console.log(i)
+    i++
+} while (i < 6);
