686903dac41049a334f3b026da93d8b91e37445d
Sahil Zalte <zaltesahil94@gmail.com>
2025-02-26 22:27:55 +0530

    Day 58

---
+++ JAVASCRIPT/DAY 58/index.js
@@ -0,0 +1,33 @@
+function Sahil(name) {
+
+    console.log("Hello From " + name + " To all ");
+}
+
+function sum(a, b, c = 1) {
+    // console.log(a + b)
+    return a + b + c
+}
+
+Sahil("Sahil")
+// Sahil("Rohan")
+// Sahil("Aman")
+
+
+result1 = sum(50, 49)
+result2 = sum(1, 2, 5)
+result3 = sum(7, 53)
+1
+console.log("The sum of these numbers is: ", result1);
+console.log("The sum of these numbers is: ", result2);
+console.log("The sum of these numbers is: ", result3);
+
+
+
+const func1 = (a) => {
+    console.log("Number is:" + a);
+
+}
+
+
+
+func1(90)
