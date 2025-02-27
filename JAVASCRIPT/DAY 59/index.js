cac8ee0c79bac7c6669d639c2057cf68ba087d8e
Sahil Zalte <zaltesahil94@gmail.com>
2025-02-27 21:07:48 +0530

    Day 59

---
+++ JAVASCRIPT/DAY 59/index.js
@@ -0,0 +1,16 @@
+a = 80
+b = 8
+
+console.log("*-----Basic Calculator----*");
+
+
+if (Math.random(1, 10))
+    console.log("Addtion is :" + a + b);
+    console.log("Subtraction is :" + (a - b));
+    console.log("Multiplication is :" + a * b);
+    console.log("Division is :" + a / b);
+
+console.log("Addtion is :" + (a - b));
+console.log("Subtraction is :" + (a / b));
+console.log("Multiplication is :" + a + b);
+console.log("Division is :" + a ** b);
