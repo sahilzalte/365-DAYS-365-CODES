6c8151bcf03a6b33a04d9411f6b4a1db38b67df9
Sahil Zalte <zaltesahil94@gmail.com>
2025-02-28 21:42:50 +0530

    Day 60

---
+++ JAVASCRIPT/DAY 60/index.js
@@ -0,0 +1,47 @@
+// console.log("Sahil")
+
+let a = "Sahil"
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+// console.log(a[5]) // <-- undefined
+
+
+console.log(a.length);
+
+
+
+let name = "Sahil"
+let Frnd = "Aman"
+// console.log("His name is " + name + " And his friend is " + Frnd);
+
+
+console.log(`His name is ${name} and his friend name id ${Frnd}`)// <-- Template literals
+
+
+let escape = "This is \"Sahil\"";
+console.log(escape);
+
+
+let b = "Shivam"
+
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+
+
+console.log(b.slice(1, 3))
+console.log(b.slice(1))
+
+console.log(b.replace("Sh", "55"))
+
+console.log(b.concat("Aman", "Amit", "rahul"))
+
+let white = "   Sahil   "
+
+console.log(white.trim());
+
+console.log(b)
