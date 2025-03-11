286a8b5f6a05d3a213506c5fe5253de8a3f19b70
Sahil Zalte <zaltesahil94@gmail.com>
2025-03-11 20:38:58 +0530

    Day 71

---
+++ JAVASCRIPT/DAY 71/index.js
@@ -0,0 +1,6 @@
+// let div = document.createElement("div")
+// div.innerHTML = "I have been inserted <b>By Sahil</b>"
+// div.setAttribute("class", "created")
+// document.querySelector(".container").append(div)
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("beforebegin", " I am Under The Water <b>By Sahil</b>")
