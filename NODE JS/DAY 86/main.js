5ab36104f315f0dbcf836adf97ec0bbfaf4ec9fb
Sahil Zalte <zaltesahil94@gmail.com>
2025-03-26 21:03:20 +0530

    Day 86

---
+++ NODE JS/DAY 86/main.js
@@ -0,0 +1,13 @@
+// import { a,b,c,d,e} from "./mymodule.js"
+
+// console.log(a)
+
+// import obj from "./mymodule.js"
+// console.log(obj)
+
+// import sahil from "./mymodule.js"
+// console.log(sahil)
+
+const a = require('./mymodule2.js');
+
+console.log(a, __dirname)
---
+++ NODE JS/DAY 86/mymodule2.js
@@ -0,0 +1,10 @@
+// module.exports ={
+//     a:3,
+//     b:42
+// }
+
+let c = 56
+
+module.exports = c
+module.exports = c
+module.exports = c
