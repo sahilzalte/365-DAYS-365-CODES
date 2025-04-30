2844479dccad4c88f2fba4d7e92b3d9824ab58bb
Sahil Zalte <zaltesahil94@gmail.com>
2025-04-30 10:50:43 +0530

    Day 121

---
+++ NEXT JS/sahil-first-app/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-xl">Problem Solved by Next.js</h1>
+            <ul className='bg-red-600 p-4 rounded-md'>
+                <li>Full stack Solution</li>
+                <li>File Based Routing</li>
+                <li>Additional Features like router from next/navigation</li>
+                <li>Optimized rendering</li>
+            </ul>
+        </div>
+    )
+}
+
+export default page
