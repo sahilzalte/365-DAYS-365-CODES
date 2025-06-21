e33dc39aad36d473f79b05c144b617cbbe23612f
Sahil Zalte <zaltesahil94@gmail.com>
2025-06-21 19:29:14 +0530

    Day 173

---
+++ REVISIT MERN/DAY 173/main.js
@@ -0,0 +1,14 @@
+
+function myFunction() {
+    if (true) {
+        const a = 'sahil'
+        // a = 56
+
+        console.log(a); 
+    }
+    // console.log(a); 
+}
+
+// console.log(a); 
+
+myFunction(); 
