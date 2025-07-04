969f1a3439eb540bae1e21ddee541f223d8f28f2
Sahil Zalte <zaltesahil94@gmail.com>
2025-07-04 22:27:29 +0530

    Day 187

---
+++ REVISIT MERN/DAY 187/app/about/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-xl text-red-600">
+                I am about Page
+                <ul className='list-none'>Full Stack Solution</ul>
+                <li>Frontend</li>
+
+                <li>Backend</li>
+                                                    
+            </h1>
+        </div>
+    )
+}
+
+export default page
---
+++ REVISIT MERN/DAY 187/app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div> I am home Page</div>
+  )
+}
+
+export default page
