9ddc29520beb1b747715e63e7aa0a18a555a3a39
Sahil Zalte <zaltesahil94@gmail.com>
2025-07-05 23:48:41 +0530

    Day 188

---
+++ REVISIT MERN/DAY 188/components/Navbar.js
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            Navbar
+            {console.log('navbar')}
+        </div>
+    )
+}
+
+export default Navbar
