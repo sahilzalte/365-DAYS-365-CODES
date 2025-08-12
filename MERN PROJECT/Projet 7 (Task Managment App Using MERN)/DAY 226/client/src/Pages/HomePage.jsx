b320d4720cda013c142e30a08aa3a89b59ca64df
Sahil Zalte <zaltesahil94@gmail.com>
2025-08-12 23:12:44 +0530

    Day 226

---
+++ MERN PROJECT/Projet 7 (Task Managment App Using MERN)/DAY 226/client/src/Pages/HomePage.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import z, { ZodError } from "zod";
+import { getZodError } from "../helper/getZodError";
+const HomePage = () => {
+
+    const [formData, setFormData] = useState();
+    const [err, setError] = useState();
+
+    const taskSchema = z.object({
+        title: z.string().min(2, "Title must be at least 3 characters long"),
+        description: z.string().min(3, "Description must be at least 3 characters long").max(500, "Description must be at most 500 characters long"),
+    });
+
+    const handleInput = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const validatedData = taskSchema.parse(formData)
+            const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/task/create-task`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(validatedData),
+            })
+
+            const responseData = await response.json();
+            console.log(responseData);
+
+        } catch (error) {
+            if (error instanceof ZodError) {
+                const getError = getZodError(error.issues)
+                setError(getError);
+            }
+            console.log(error);
+
+        }
+
+    }
+    return (
+        <div className="pt-5">
+            <h1 className="text-2xl font-bold mb-5">Add Task</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-5">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 ">
+                        Title
+                    </label>
+                    <input onChange={handleInput}
+                        name="title"
+                        type="text"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                        placeholder="Task title"
+                        required
+                    />
+                    {err && err.title && <span className="text-red-600 text-sm">{err.title}</span>}
+                </div>
+                <div className="mb-5">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 ">
+                        Description
+                    </label>
+                    <textarea onChange={handleInput}
+                        name="description"
+                        rows="4"
+                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Task description..."
+                    ></textarea>
+                    {err && err.description && <span className="text-red-600 text-sm">{err.description}</span>}
+                </div>
+
+                <button
+                    type="submit"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default HomePage;
