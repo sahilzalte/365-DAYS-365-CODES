6e86f7dbb8809f74b29c21db98606c0b38615117
Sahil Zalte <zaltesahil94@gmail.com>
2025-08-18 22:00:28 +0530

    Day 232

---
+++ MERN PROJECT/Project 8 (Agency Managment App Using React.js)/DAY 232/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+
+const App = () => {
+  const [theme, setTheme] = useState('light')
+  return (
+    <>
+      <div className='dark:bg-black relative'>
+        <Navbar theme={theme} setTheme={setTheme} />
+      </div>
+    </>
+  )
+}
+
+export default App
