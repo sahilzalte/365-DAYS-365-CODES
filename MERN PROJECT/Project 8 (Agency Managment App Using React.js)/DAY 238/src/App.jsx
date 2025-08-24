a95a1e184a5d0f8495cf3ec5207a5af9cba2a8ce
Sahil Zalte <zaltesahil94@gmail.com>
2025-08-24 22:57:00 +0530

    Day 238

---
+++ MERN PROJECT/Project 8 (Agency Managment App Using React.js)/DAY 238/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
+import Services from './components/Services'
+import OurWork from './components/OurWork'
+
+
+const App = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
+  return (
+    <>
+      <div className='dark:bg-black relative'>
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Hero />
+        <TrustedBy />
+        <Services />
+        <OurWork />
+      </div>
+    </>
+  )
+}
+
+export default App
