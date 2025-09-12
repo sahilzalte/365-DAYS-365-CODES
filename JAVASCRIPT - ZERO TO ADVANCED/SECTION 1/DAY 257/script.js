63aa78be93fb8dd60910a7468f51d62e7093097c
Sahil Zalte <zaltesahil94@gmail.com>
2025-09-12 21:23:38 +0530

    Day 257

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 257/script.js
@@ -0,0 +1,17 @@
+// Basic Exercise
+
+// console.log(age);
+let age = 25;
+
+var x = 1
+{
+    var x = 2;
+}
+// console.log(x);
+
+let a = 1;
+{   
+    let a = 2;
+    console.log("Inside block: " + a);
+}
+console.log("Outside block: " + a);
