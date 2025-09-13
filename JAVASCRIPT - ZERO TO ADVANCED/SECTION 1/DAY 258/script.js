5048acec41e01335aec1a483b5d13841cea53520
Sahil Zalte <zaltesahil94@gmail.com>
2025-09-13 17:03:08 +0530

    Day 258

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 258/script.js
@@ -0,0 +1,12 @@
+// Data Types
+
+
+// 1. Primitive Data Types
+let age = 30;
+let price = 19.99;
+let name = "Alice";
+let isStudent = true;
+let bigNumber = 9007199254740991n;
+let uniqueId = Symbol('id');
+let notDefined;
+let emptyValue = null;
