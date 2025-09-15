862efa64b91a91aafc0d43308f3f88f097fbd367
Sahil Zalte <zaltesahil94@gmail.com>
2025-09-15 21:10:09 +0530

    Day 259

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 259/script.js
@@ -0,0 +1,8 @@
+//  string, number, boolean, undefined, null, symbol, bigint
+// object, array, function
+
+let name = "John"; // string
+let age = 30; // number
+let isStudent = false; // boolean
+let address; // undefined
+let phone = null; // null
