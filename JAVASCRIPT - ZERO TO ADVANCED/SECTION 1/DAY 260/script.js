4e491e3f3bcbb15b13d6de7ea399c52c915e3458
Sahil Zalte <zaltesahil94@gmail.com>
2025-09-15 21:22:15 +0530

    Day 260

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 260/script.js
@@ -0,0 +1,14 @@
+//  string, number, boolean, undefined, null, symbol, bigint
+// object, array, function
+
+let name = "John"; // string
+let age = 30; // number
+let isStudent = false; // boolean
+let address; // undefined
+let phone = null; // null
+let id = Symbol("id"); // symbol
+let bigNumber = 1234567890123456789012345678901234567890n; // bigint
+
+let person = { name: "John", age: 30 }; // object
+let numbers = [1, 2, 3, 4, 5]; // array  
+function greet() { console.log("Hello!"); } // function
