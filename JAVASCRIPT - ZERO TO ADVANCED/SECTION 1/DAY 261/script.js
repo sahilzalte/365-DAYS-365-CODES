e243fbd8b5f17d3fb4180a2b60fd6d15790a86d0
Sahil Zalte <zaltesahil94@gmail.com>
2025-09-16 21:00:45 +0530

    Day 261

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 261/script.js
@@ -0,0 +1,26 @@
+// object, array, function
+
+// array
+let a = [1, 2, 3]
+let b = a
+
+// object
+let obj1 = { name: 'Alice', age: 25 }
+let obj2 = obj1
+
+// function
+function greet() {
+    console.log('Hello!')
+}
+
+
+// Dynamic typing
+let variable = 42          // number
+variable = 'Hello'        // string
+variable = true           // boolean
+variable = { key: 'value' } // object
+variable = [1, 2, 3]     // array
+
+// typeof operator
+console.log(typeof 42)            // "number"
+console.log(typeof 'Hello')       // "string"
