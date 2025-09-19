dd51be88a78592ce23fb57e71c35b0be06895031
Sahil Zalte <zaltesahil94@gmail.com>
2025-09-19 18:48:08 +0530

    Day 263

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 263/script.js
@@ -0,0 +1,44 @@
+// Arithmatic Operators
+// + - / * % ** 
+
+// +
+let a = 10;
+let b = 20;
+console.log(a + b);
+
+// 'Sahil' + ' dev'
+console.log('Sahil' + ' dev');
+console.log('Sahil' + 10);
+console.log('10' + 10);
+console.log(10 + '10');
+
+// -
+console.log(10 - '5'); // 5
+console.log('10' - 5);
+console.log('10' - '5');
+console.log('Sahil' - 'dev'); // NaN
+console.log('10' - 'dev'); // NaN
+
+// *
+console.log(5 * '2'); // 10
+console.log('5' * '2'); // 10
+console.log('Sahil' * 'dev'); // NaN
+
+// /
+console.log(10 / '2'); // 5
+console.log('10' / '2'); // 5
+console.log('Sahil' / 'dev'); // NaN
+console.log(10 / 0); // Infinity
+console.log(-10 / 0); // -Infinity
+console.log(0 / 0); // NaN
+
+// %
+console.log(10 % 3); // 1
+console.log(10 % 2); // 0
+console.log(5 % 2); // 1
+console.log(5 % 0); // NaN 
+
+// **
+console.log(2 ** 3); // 8
+console.log(3 ** 3); // 27
+console.log(4 ** 0.5); // 2
