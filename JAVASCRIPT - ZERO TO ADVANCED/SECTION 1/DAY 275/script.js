d2b6192c1acb81a662b4d186d23ec09ceeed4460
Sahil Zalte <zaltesahil94@gmail.com>
2025-10-03 21:53:28 +0530

    Day 275

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 275/script.js
@@ -0,0 +1,30 @@
+// Q1
+for (let i = 1; i <= 25; i++) {
+    // console.log(i);  // Print numbers from 1 to 25
+}
+
+// Q2
+for (let i = 50; i >= 25; i--) {
+    // console.log(i);  // Print numbers from 50 down to 25
+}
+
+// Q3
+let j = 1;
+while (j <= 20) {
+    // console.log(j);  // Print numbers from 1 to 20 using while
+    j++;
+}
+
+// Q4
+let k = 10;
+while (k >= 1) {
+    // console.log(k);  // Print numbers from 10 to 1 using while
+    k--;
+}
+
+// Q5
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 == 0) {
+        // console.log(i);  // Print even numbers between 1 and 30
+    }
+}
