35223e74ac86afa953df49ebd5a06b974b744459
Sahil Zalte <zaltesahil94@gmail.com>
2025-10-03 21:55:29 +0530

    Day 277

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 277/script.js
@@ -0,0 +1,31 @@
+// Q11
+for (let i = 1; i <= 50; i++) {
+    if (i % 7 == 0) {
+        // console.log(i);  // Print multiples of 7 up to 50
+    }
+}
+
+// Q12
+const table = 9;
+for (let i = 1; i <= 10; i++) {
+    // console.log(`${table} x ${i} = ${table * i}`);  // Table of 9
+}
+
+// Q13
+let n = 1;
+do {
+    // console.log(n);  // Print numbers 1 to 10 using do-while
+    n++;
+} while (n <= 10);
+
+// Q14
+for (let i = 1; i <= 20; i++) {
+    if (i === 10) break;
+    // console.log(i);  // Stop when number reaches 10
+}
+
+// Q15
+for (let i = 1; i <= 20; i++) {
+    if (i === 10) continue;
+    // console.log(i);  // Skip 10, print others
+}
