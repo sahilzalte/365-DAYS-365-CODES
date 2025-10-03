2b4ce549185c66c301a06d3b3d9c073d461c885e
Sahil Zalte <zaltesahil94@gmail.com>
2025-10-03 21:54:28 +0530

    Day 276

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 276/script.js
@@ -0,0 +1,30 @@
+// Q6
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 != 0) {
+        // console.log(i);  // Print odd numbers between 1 and 30
+    }
+}
+
+// Q7
+let total = 0;
+for (let i = 1; i <= 100; i++) {
+    total += i;
+}
+// console.log(total);  // Print sum of numbers from 1 to 100
+
+// Q8
+let product = 1;
+for (let i = 1; i <= 10; i++) {
+    product *= i;
+}
+// console.log(product);  // Print factorial of 10
+
+// Q9
+for (let i = 1; i <= 15; i++) {
+    // console.log(i * i);  // Print squares of 1 to 15
+}
+
+// Q10
+for (let i = 1; i <= 12; i++) {
+    // console.log(i * i * i);  // Print cubes of 1 to 12
+}
