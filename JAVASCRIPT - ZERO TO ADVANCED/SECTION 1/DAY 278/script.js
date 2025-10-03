e6b425b17ad3b78f3ce92bc1f3a21de74986f925
Sahil Zalte <zaltesahil94@gmail.com>
2025-10-03 21:55:56 +0530

    Day 278

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 278/script.js
@@ -0,0 +1,33 @@
+// Q16
+for (let i = 1; i <= 5; i++) {
+    // console.log(i * 2);  // Print: 2, 4, 6, 8, 10
+}
+
+// Q17
+for (let i = 1; i <= 3; i++) {
+    if (i === 1) {
+        // console.log("apple");
+    } else if (i === 2) {
+        // console.log("mango");
+    } else if (i === 3) {
+        // console.log("grape");
+    }
+}
+
+// Q18
+let text = "HELLO";
+for (let i = 0; i < text.length; i++) {
+    // console.log(text[i]);  // Print each character of HELLO
+}
+
+// Q19
+let sum = 0;
+for (let i = 5; i <= 20; i += 5) {
+    sum += i;
+}
+// console.log(sum);  // Sum = 50
+
+// Q20
+for (let i = 10; i <= 100; i += 10) {
+    // console.log(i);  // Print: 10, 20, 30, ... 100
+}
