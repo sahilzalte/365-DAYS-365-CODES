5384a2a6235914f58d603747b3b49017a0979146
Sahil Zalte <zaltesahil94@gmail.com>
2025-10-15 12:44:50 +0530

    Day 289

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 289/script.js
@@ -0,0 +1,32 @@
+// 36. Print numbers from 10 to 1 using a while loop
+let j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
+
+// 37. Print numbers 1 to 5 using do...while loop
+let k = 1;
+do {
+    console.log(k);
+    k++;
+} while (k <= 5);
+
+// 38. Print first 5 multiples of 2 using for loop
+for (let i = 1; i <= 5; i++) {
+    console.log(i * 2); // 2 4 6 8 10
+}
+
+// 39. Sum of numbers from 1 to 5
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum += i;
+}
+console.log(sum); // 15
+
+// 40. Find factorial of 5
+let factorial = 1;
+for (let i = 1; i <= 5; i++) {
+    factorial *= i;
+}
+console.log(factorial); // 120
