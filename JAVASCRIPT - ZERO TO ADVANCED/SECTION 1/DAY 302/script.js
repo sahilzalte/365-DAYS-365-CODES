499ad4d836cc93513c87680d280bb49b94509c32
Sahil Zalte <zaltesahil94@gmail.com>
2025-10-28 20:51:52 +0530

    Day 302

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 1/DAY 302/script.js
@@ -0,0 +1,9 @@
+// Create array
+const fruits = ['apple', 'banana', 'cherry'];
+
+// Access array element
+console.log(fruits[1]); // Output: banana
+
+// modified array
+fruits[1] = 'blueberry';
+console.log(fruits); // Output: ['apple', 'blueberry', 'cherry']
