b14d01bdb1f700fce447491fe24dcac193267e23
Sahil Zalte <zaltesahil94@gmail.com>
2025-11-03 11:30:31 +0530

    Day 309

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 2/DAY 309/script.js
@@ -0,0 +1,6 @@
+let h1  = document.querySelector('h1')
+h1.style.color = 'blue'
+h1.style.backgroundColor = 'yellow'
+h1.classList.add('hulu')
+h1.classList.remove('hulu')
+h1.classList.toggle('hulu')
