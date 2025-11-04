a587c174cc467514d892e3726a279dd7c1a61c00
Sahil Zalte <zaltesahil94@gmail.com>
2025-11-04 22:42:46 +0530

    Day 311

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 2/DAY 311/script.js
@@ -0,0 +1,10 @@
+// Q.3 Select a paragraph and replace its content with:
+// {<b>Updated</b> by JavaScript}
+
+let a = document.querySelector("p")
+a.innerHTML='<b>Updated</b> by JavaScript'
+
+// Q.4 Select a link and update its href to point to https://codebysahil.me
+
+let a = document.getElementById("myLink")
+a.href="https://codebysahil.me"
