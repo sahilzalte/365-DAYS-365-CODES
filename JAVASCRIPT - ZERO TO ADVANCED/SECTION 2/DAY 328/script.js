22b00dfb80ec26515407969819984622fff52788
Sahil Zalte <zaltesahil94@gmail.com>
2025-11-22 19:29:47 +0530

    DAY 328

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 2/DAY 328/script.js
@@ -0,0 +1,15 @@
+// SetTimeout example
+let time = setTimeout(() => {
+    // console.log('Suiiiii');
+}, 5000);
+
+// ClearTimeout example
+clearTimeout(tm);
+
+// SetInterval example
+let interval = setInterval(() => {
+    // console.log('Suiiiii');
+}, 5000);
+    
+// ClearInterval example
+clearInterval(interval);
