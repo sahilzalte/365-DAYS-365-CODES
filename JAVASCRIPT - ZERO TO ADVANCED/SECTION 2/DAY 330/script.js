2e180ed1802db1e3ba59cc6d0d9010a293b286dd
Sahil Zalte <zaltesahil94@gmail.com>
2025-11-23 23:54:26 +0530

    Day 330

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 2/DAY 330/script.js
@@ -0,0 +1,15 @@
+// LocalStorage
+
+// Store data
+localStorage.setItem('name','Sahiil')
+
+// Fetch data
+let val =localStorage.getItem('name')
+// console.log(val);
+
+// Remove data
+localStorage.removeItem('name')
+
+// Update data
+localStorage.setItem('name','John Doe')
+
