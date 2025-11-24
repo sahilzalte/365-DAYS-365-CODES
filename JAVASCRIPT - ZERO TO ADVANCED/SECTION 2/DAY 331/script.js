f9e54d73697be3bbc7164430681e98b429e0d242
Sahil Zalte <zaltesahil94@gmail.com>
2025-11-24 22:12:30 +0530

    Day 331

---
+++ JAVASCRIPT - ZERO TO ADVANCED/SECTION 2/DAY 331/script.js
@@ -0,0 +1,22 @@
+// Session Storage
+
+// Store data
+sessionStorage.setItem('name','Sahiil')
+
+// Fetch data
+let val =sessionStorage.getItem('name')
+// console.log(val);
+
+// Remove data
+sessionStorage.removeItem('name')
+
+// Update data
+sessionStorage.setItem('name','John Doe')
+
+// Clear all data
+sessionStorage.clear()
+
+
+// Cookies
+
+document.cookie = "Pass=suii"
